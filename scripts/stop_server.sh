38842415c40070a5a6259e65ecfa98b8e41387db
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-12 16:01:46 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-pkill -f "spring-petclinic-3.3.0-SNAPSHOT.jar"
+check = $(ps aux | grep spring-petclinic-3.3.0-SNAPSHOT.jar)
 
+if [[$check -eq 0]]; then
+    echo "Application is running"
+    pkill -f spring-petclinic-3.3.0-SNAPSHOT.jar
+    exit 0
+else
+    echo "Apache failed to stop"
+    exit 1
+fi
