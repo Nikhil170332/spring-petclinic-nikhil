ca822c1bd75b4633cbf2dbf298f9fc72d53e22d4
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-13 11:42:09 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #Initializing two variables 
-a=$(yum | grep "command not found") 
+a=$(apt | grep "command not found") 
 
 #Check whether they are not equal 
-if [ $a != "command not found" ] 
-then 
-	echo "Ubuntu Instance, using apt command"
-	sudo apt install openjdk-17-jre-headless -y
+if [[$a -eq command not found]]; then
+        echo "Linux Machine, Using yum command"
+	sudo yum install java-17-amazon-corretto.x86_64 -y
 else
-    echo "Linux Machine, Using yum command"
-	sudo apt install java-17-amazon-corretto.x86_64 -y
+        echo "Ubuntu Instance, using apt command"
+        sudo apt install openjdk-17-jre-headless -y
 fi 
 
