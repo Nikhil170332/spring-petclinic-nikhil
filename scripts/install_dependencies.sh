45654aa28bb74c4651d3028648f2de9896665448
Nikhil170332 <107186916+Nikhil170332@users.noreply.github.com>
2024-08-13 11:21:16 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-echo "Checking Java running"
-sudo yum install java-17-amazon-corretto.x86_64 -y
-java -version
+#Initializing two variables 
+a=$(yum | grep "command not found") 
+
+#Check whether they are not equal 
+if [ $a != "command not found" ] 
+then 
+	echo "Ubuntu Instance, using apt command"
+	sudo apt install openjdk-17-jre-headless -y
+else
+    echo "Linux Machine, Using yum command"
+	sudo apt install java-17-amazon-corretto.x86_64 -y
+fi 
 
